f32b2ccdc23e880b182976eabce204ffa739cdb8
Rezuan Kassim <rezuankassim@hotmail.com>
2025-09-22 13:58:34 +0800

    remove index route

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\OrderHistoryController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::redirect('/', '/login');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
