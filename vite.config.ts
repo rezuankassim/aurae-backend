2d131939a771b062551a02b337a2f5ecf8af0850
Rezuan Kassim <rezuankassim@hotmail.com>
2025-09-24 13:12:36 +0800

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -17,6 +18,11 @@ export default defineConfig({
             formVariants: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'), // 👈 '@' → resources/js
+        },
+    },
     esbuild: {
         jsx: 'automatic',
     },
