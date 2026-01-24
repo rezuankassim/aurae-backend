0efced4955f9086efdf1adc086425de28b60f373
Rezuan Kassim <rezuankassim@hotmail.com>
2026-01-24 22:04:36 +0800

    fix logo issue

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,7 +1,7 @@
 export default function AppLogoIcon() {
     return (
         <div className="size-9 rounded-full bg-black dark:bg-transparent">
-            <img src="logo.png" alt="App Logo" />
+            <img src="/logo.png" alt="App Logo" />
         </div>
     );
 }
