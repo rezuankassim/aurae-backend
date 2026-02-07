b0924c01b22bd01ee096f9be78eb094585ac9007
Rezuan Kassim <rezuankassim@hotmail.com>
2026-02-07 16:16:46 +0800

    fixes

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -24,8 +24,8 @@ class TransactionResource extends BaseResource
             'card_type' => $this->card_type,
             'last_four' => $this->last_four,
             'notes' => $this->notes,
-            'captured_at' => $this->captured_at?->toIso8601String(),
-            'created_at' => $this->created_at->toIso8601String(),
+            'captured_at' => $this->captured_at instanceof \Carbon\Carbon ? $this->captured_at->toIso8601String() : $this->captured_at,
+            'created_at' => $this->created_at instanceof \Carbon\Carbon ? $this->created_at->toIso8601String() : $this->created_at,
         ];
     }
 }
