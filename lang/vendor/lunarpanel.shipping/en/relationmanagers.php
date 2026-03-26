e9b0ae508a7fc4d3724d09963c8f6a84a7906308
Rezuan Kassim <rezuankassim@hotmail.com>
2026-03-27 00:15:05 +0800

    fixes label

---
+++ lang/vendor/lunarpanel.shipping/en/relationmanagers.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'shipping_rates' => [
+        'form' => [
+            'prices' => [
+                'repeater' => [
+                    'min_weight' => [
+                        'label' => 'Min. Weight (kg)',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
