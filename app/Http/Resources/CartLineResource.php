5bfe9976dbcea597d0588816452ad86026eda02a
Rezuan Kassim <rezuankassim@hotmail.com>
2026-03-29 13:40:55 +0800

    fixes

--- app/Http/Resources/CartLineResource.php
+++ app/Http/Resources/CartLineResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Lunar\Facades\Pricing;
 
 class CartLineResource extends BaseResource
 {
@@ -25,7 +26,32 @@ class CartLineResource extends BaseResource
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'purchaseable' => $this->whenLoaded('purchasable', function () {
-                return ProductVariantResource::make($this->purchasable);
+                $data = ProductVariantResource::make($this->purchasable)->toArray(request());
+
+                if ($this->purchasable->relationLoaded('product') && $this->purchasable->product?->relationLoaded('variants')) {
+                    $data['variants'] = $this->purchasable->product->variants->map(fn ($variant) => [
+                        'id' => $variant->id,
+                        'sku' => $variant->sku,
+                        'thumbnail' => [
+                            'url' => $variant->getThumbnail()?->getUrl(),
+                        ],
+                        'price' => Pricing::for($variant)->get()->matched?->price->formatted(),
+                        'description' => $variant->getDescription(),
+                        'values' => $variant->values->map(fn ($value) => [
+                            'id' => $value->id,
+                            'product_option_id' => $value->product_option_id,
+                            'name' => $value->translate('name'),
+                            'position' => $value->position,
+                            'option' => [
+                                'id' => $value->option->id,
+                                'name' => $value->option->translate('name'),
+                            ],
+                        ]),
+                        'values_ids' => $variant->values->pluck('id'),
+                    ]);
+                }
+
+                return $data;
             }),
             'product' => $this->whenLoaded('purchasable', function () {
                 if ($this->purchasable->relationLoaded('product')) {
