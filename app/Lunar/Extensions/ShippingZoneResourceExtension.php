531dc517bd2003267373f40173f060ca46060e4d
Rezuan Kassim <rezuankassim@hotmail.com>
2026-04-02 21:21:34 +0800

    fixes

--- app/Lunar/Extensions/ShippingZoneResourceExtension.php
+++ app/Lunar/Extensions/ShippingZoneResourceExtension.php
@@ -2,9 +2,15 @@
 
 namespace App\Lunar\Extensions;
 
+use App\Lunar\Pages\ManageShippingRatesPage;
 use Lunar\Admin\Support\Extending\ResourceExtension;
 
 class ShippingZoneResourceExtension extends ResourceExtension
 {
-    //
+    public function extendPages(array $pages): array
+    {
+        $pages['rates'] = ManageShippingRatesPage::route('/{record}/rates');
+
+        return $pages;
+    }
 }
