6013c798d7159d6cf33322fec33636c969254642
Rezuan Kassim <rezuankassim@hotmail.com>
2026-05-21 20:52:00 +0800

    fixes

--- app/Lunar/Extensions/CollectionGroupCreateExtension.php
+++ app/Lunar/Extensions/CollectionGroupCreateExtension.php
@@ -4,6 +4,7 @@ namespace App\Lunar\Extensions;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Illuminate\Support\Str;
 use Lunar\Admin\Support\Extending\CreatePageExtension;
 
 class CollectionGroupCreateExtension extends CreatePageExtension
@@ -14,6 +15,13 @@ class CollectionGroupCreateExtension extends CreatePageExtension
         $this->hideHandleField($schema);
 
         return $form->schema($schema);
+    }
+
+    public function beforeCreate(array $data): array
+    {
+        $data['handle'] = Str::slug($data['name']);
+
+        return $data;
     }
 
     protected function hideHandleField(array $components): void
